4423a2fea45f7bb8b1d7bf5a6120eb0bb71f75d2
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-07-20 17:37:48 +0500

    Create return.py

---
+++ return.py
@@ -0,0 +1,21 @@
+#!1 return 
+
+def yigindi(a,b,c):
+	return(a*b+c)
+natija = yigindi(1,3,5)
+print(natija) #8
+
+son= int(input("Sonni kiriting:")) 
+def tekshir(son):
+  if son < 0:
+  	return "Manfiy"
+  else:
+  	return "Musbat"
+print(tekshir(son))
+  """
+Natija:
+	
+Sonni kiriting:7
+Musbat
+
+"""
