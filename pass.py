b75d9bd36e6d2e0936cca6aecca773ff5217afc0
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-07-21 17:51:35 +0500

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,9 @@
+#!2. pass - hech narsa qilmaydi shunchaki joy toldiruvchi.
+def vaxtincha():
+	pass #keyin yozaman
+class son:
+	pass #bo'sh class
+for son  in range(21):
+	if son == 2:
+		pass # hech narsa qilma
+print(son) # 20
