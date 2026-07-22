b031cde7e87a27c18ad20d9840dc860ca3105a25
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-07-22 15:03:11 +0500

    Create None.py

---
+++ None.py
@@ -0,0 +1,12 @@
+#3!. None - bo'sh qiymat hech narsani ifodalaydi.Return yozilmasa funksiya avtomatiski None qaytaradi
+
+def ism():
+	print("Qunduz") #Qunduz
+#return yo'q None qaytaradi
+
+natija = ism()
+print(natija)# None
+
+x = None
+if x is None:
+	print("Qiymat yo'q") #Qiymat yo'q
