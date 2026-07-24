c8c299e09e250b846ed21eb2c552097ba59552df
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-07-24 05:08:48 +0500

    Create hammasi-birda.py

---
+++ hammasi-birda.py
@@ -0,0 +1,7 @@
+def ishchi(ism= None):
+	if ishchi is None: # qiymat yo'q'
+		pass # bo'sh qiymat'
+		return "Noma'lum" #qiymat qaytarish
+	return f"Hurmatli {ism}!"
+print(ishchi()) # Hurmatli Noma'lum!
+print(ishchi("Aziz")) # Hurmatli Aziz!
